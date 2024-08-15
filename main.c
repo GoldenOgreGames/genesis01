0cdf4d77d3c038fa56c87f86771095fc94d757dd
Damian Depuy <goldenogregames@gmail.com>
2024-08-15 18:25:22 -0400

    Added handleInput

--- main.c
+++ main.c
@@ -8,19 +8,36 @@
     int player_y = 10;
     int player_vel = 2; // velocity
 
+static void handleInput();
 int main(){
     
     SPR_init();
     PAL_setPalette(PAL2, player1_sp.palette->data, DMA);
     player = SPR_addSprite(&player1_sp, player_x, player_y, TILE_ATTR(PAL2, FALSE, FALSE, FALSE));
     // will remove
-  VDP_drawText("Player X: ", 0, 0);
+    VDP_drawText("Player X: ", 0, 0);
     
 
     while(1){
-        SPR_setPosition(player,player_x, player_y);
+        handleInput();
         SPR_update();
         SYS_doVBlankProcess();
     }
     return 0;
+}
+
+static void handleInput(){
+    
+	u16 value = JOY_readJoypad(JOY_1);
+	if(value & BUTTON_RIGHT){
+		player_x += player_vel;
+	} else if(value & BUTTON_LEFT){
+		player_x -= player_vel;
+	}
+	if(value & BUTTON_UP){
+		player_y -= player_vel;
+	} else if(value & BUTTON_DOWN){
+		player_y += player_vel;
+	}
+    SPR_setPosition(player,player_x, player_y);
 }
--- out/main.d
+++ out/main.d
@@ -1,30 +1,30 @@
-out/main.o: main.c C:/SEGA_DEV/SGDK/inc/genesis.h \
- C:/SEGA_DEV/SGDK/inc/types.h C:/SEGA_DEV/SGDK/inc/config.h \
- C:/SEGA_DEV/SGDK/inc/asm.h C:/SEGA_DEV/SGDK/inc/sys.h \
- C:/SEGA_DEV/SGDK/inc/sram.h C:/SEGA_DEV/SGDK/inc/mapper.h \
- C:/SEGA_DEV/SGDK/inc/memory.h C:/SEGA_DEV/SGDK/inc/memory_base.h \
- C:/SEGA_DEV/SGDK/inc/tools.h C:/SEGA_DEV/SGDK/inc/bmp.h \
- C:/SEGA_DEV/SGDK/inc/maths.h C:/SEGA_DEV/SGDK/inc/vdp.h \
- C:/SEGA_DEV/SGDK/inc/pal.h C:/SEGA_DEV/SGDK/inc/dma.h \
- C:/SEGA_DEV/SGDK/inc/vdp_tile.h C:/SEGA_DEV/SGDK/inc/vdp_bg.h \
- C:/SEGA_DEV/SGDK/inc/map.h C:/SEGA_DEV/SGDK/inc/pool.h \
- C:/SEGA_DEV/SGDK/inc/object.h C:/SEGA_DEV/SGDK/inc/font.h \
- C:/SEGA_DEV/SGDK/res/libres.h C:/SEGA_DEV/SGDK/inc/string.h \
- C:/SEGA_DEV/SGDK/inc/tab_cnv.h C:/SEGA_DEV/SGDK/inc/maths3D.h \
- C:/SEGA_DEV/SGDK/inc/vdp_spr.h C:/SEGA_DEV/SGDK/inc/vdp_pal.h \
- C:/SEGA_DEV/SGDK/inc/vram.h C:/SEGA_DEV/SGDK/inc/sprite_eng.h \
- C:/SEGA_DEV/SGDK/inc/sprite_eng_legacy.h C:/SEGA_DEV/SGDK/inc/z80_ctrl.h \
- C:/SEGA_DEV/SGDK/inc/ym2612.h C:/SEGA_DEV/SGDK/inc/psg.h \
- C:/SEGA_DEV/SGDK/inc/snd/sound.h C:/SEGA_DEV/SGDK/inc/snd/xgm.h \
- C:/SEGA_DEV/SGDK/inc/snd/xgm2.h C:/SEGA_DEV/SGDK/inc/snd/smp_null.h \
- C:/SEGA_DEV/SGDK/inc/snd/smp_null_dpcm.h \
- C:/SEGA_DEV/SGDK/inc/snd/pcm/snd_pcm.h \
- C:/SEGA_DEV/SGDK/inc/snd/pcm/snd_dpcm2.h \
- C:/SEGA_DEV/SGDK/inc/snd/pcm/snd_pcm4.h C:/SEGA_DEV/SGDK/inc/joy.h \
- C:/SEGA_DEV/SGDK/inc/timer.h C:/SEGA_DEV/SGDK/inc/task.h \
- C:/SEGA_DEV/SGDK/inc/task_cst.h \
- C:/SEGA_DEV/SGDK/inc/ext/flash-save/flash.h C:/SEGA_DEV/SGDK/inc/types.h \
- C:/SEGA_DEV/SGDK/inc/ext/flash-save/saveman.h \
- C:/SEGA_DEV/SGDK/inc/ext/console.h C:/SEGA_DEV/SGDK/inc/config.h \
- C:/SEGA_DEV/SGDK/inc/string.h C:/SEGA_DEV/SGDK/inc/maths.h \
- C:/SEGA_DEV/SGDK/inc/dma.h res/resources.h
+out/main.o: main.c D:/SEGA_DEV/SGDK/inc/genesis.h \
+ D:/SEGA_DEV/SGDK/inc/types.h D:/SEGA_DEV/SGDK/inc/config.h \
+ D:/SEGA_DEV/SGDK/inc/asm.h D:/SEGA_DEV/SGDK/inc/sys.h \
+ D:/SEGA_DEV/SGDK/inc/sram.h D:/SEGA_DEV/SGDK/inc/mapper.h \
+ D:/SEGA_DEV/SGDK/inc/memory.h D:/SEGA_DEV/SGDK/inc/memory_base.h \
+ D:/SEGA_DEV/SGDK/inc/tools.h D:/SEGA_DEV/SGDK/inc/bmp.h \
+ D:/SEGA_DEV/SGDK/inc/maths.h D:/SEGA_DEV/SGDK/inc/vdp.h \
+ D:/SEGA_DEV/SGDK/inc/pal.h D:/SEGA_DEV/SGDK/inc/dma.h \
+ D:/SEGA_DEV/SGDK/inc/vdp_tile.h D:/SEGA_DEV/SGDK/inc/vdp_bg.h \
+ D:/SEGA_DEV/SGDK/inc/map.h D:/SEGA_DEV/SGDK/inc/pool.h \
+ D:/SEGA_DEV/SGDK/inc/object.h D:/SEGA_DEV/SGDK/inc/font.h \
+ D:/SEGA_DEV/SGDK/res/libres.h D:/SEGA_DEV/SGDK/inc/string.h \
+ D:/SEGA_DEV/SGDK/inc/tab_cnv.h D:/SEGA_DEV/SGDK/inc/maths3D.h \
+ D:/SEGA_DEV/SGDK/inc/vdp_spr.h D:/SEGA_DEV/SGDK/inc/vdp_pal.h \
+ D:/SEGA_DEV/SGDK/inc/vram.h D:/SEGA_DEV/SGDK/inc/sprite_eng.h \
+ D:/SEGA_DEV/SGDK/inc/sprite_eng_legacy.h D:/SEGA_DEV/SGDK/inc/z80_ctrl.h \
+ D:/SEGA_DEV/SGDK/inc/ym2612.h D:/SEGA_DEV/SGDK/inc/psg.h \
+ D:/SEGA_DEV/SGDK/inc/snd/sound.h D:/SEGA_DEV/SGDK/inc/snd/xgm.h \
+ D:/SEGA_DEV/SGDK/inc/snd/xgm2.h D:/SEGA_DEV/SGDK/inc/snd/smp_null.h \
+ D:/SEGA_DEV/SGDK/inc/snd/smp_null_dpcm.h \
+ D:/SEGA_DEV/SGDK/inc/snd/pcm/snd_pcm.h \
+ D:/SEGA_DEV/SGDK/inc/snd/pcm/snd_dpcm2.h \
+ D:/SEGA_DEV/SGDK/inc/snd/pcm/snd_pcm4.h D:/SEGA_DEV/SGDK/inc/joy.h \
+ D:/SEGA_DEV/SGDK/inc/timer.h D:/SEGA_DEV/SGDK/inc/task.h \
+ D:/SEGA_DEV/SGDK/inc/task_cst.h \
+ D:/SEGA_DEV/SGDK/inc/ext/flash-save/flash.h D:/SEGA_DEV/SGDK/inc/types.h \
+ D:/SEGA_DEV/SGDK/inc/ext/flash-save/saveman.h \
+ D:/SEGA_DEV/SGDK/inc/ext/console.h D:/SEGA_DEV/SGDK/inc/config.h \
+ D:/SEGA_DEV/SGDK/inc/string.h D:/SEGA_DEV/SGDK/inc/maths.h \
+ D:/SEGA_DEV/SGDK/inc/dma.h res/resources.h
