8bb017bd03fd603c66eb3c1fcc3b3f5a9280119f
GoldenOgreGames <goldenogregames@gmail.commake>
2024-08-15 10:31:39 -0400

    Handle Input

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
     int player_x = 10; // xy coordinates
     int player_y = 10;
     int player_vel = 2; // velocity
-
+static void handleInput();
 int main(){
     
     SPR_init();
@@ -18,9 +18,34 @@ int main(){
     
 
     while(1){
-        SPR_setPosition(player,player_x, player_y);
+        //SPR_setPosition(player,player_x, player_y);
+        handleInput();
         SPR_update();
         SYS_doVBlankProcess();
     }
     return 0;
+}
+
+// handle movement
+static void handleInput(){
+    u16 value = JOY_readJoypad(JOY_1);
+    if(value & BUTTON_LEFT){
+        player_x -=player_vel;
+       // SPR_setAnim(player,ANIM_WALK);
+        SPR_setHFlip(player,TRUE);
+    } else if(value & BUTTON_RIGHT){
+        player_x +=player_vel;
+      //  SPR_setAnim(player,ANIM_WALK);
+        SPR_setHFlip(player,FALSE);
+    }
+    if(value & BUTTON_UP){
+        player_y -=player_vel;
+    } else if(value & BUTTON_DOWN){
+        player_y +=player_vel;
+    }
+
+    // will need to reset idle when anim is used
+
+    // Move sprite
+    SPR_setPosition(player, player_x, player_y);
 }
